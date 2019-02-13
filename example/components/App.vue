5e9ae2a0081568690311b04a3e28dee349ed4078
panhezeng <panhezeng@gmail.com>
2019-02-13 11:28:19 +0800

    tinymce升级到5

--- example/components/App.vue
+++ example/components/App.vue
@@ -1,59 +1,60 @@
 <template>
   <div id="app">
     <h1>VueTinymce example</h1>
-    <button @click="show=!show">点击加载移除编辑器，测试初始化和销毁：：：{{show?'销毁':'加载'}}</button>
-    <br/>
-    <br/>
+    <button @click="show = !show">
+      点击加载移除编辑器，测试初始化和销毁：：：{{ show ? "销毁" : "加载" }}
+    </button>
+    <br />
+    <br />
     <button @click="switchLanguage">点击切换语言，测试更新config</button>
-    <br/>
-    <br/>
-    <vue-tinymce :content.sync="content" :config="config" v-if="show"/>
-    <vue-tinymce :content.sync="content" :config="config"/>
+    <br />
+    <br />
+    <vue-tinymce :content.sync="content" :config="config" v-if="show" />
+    <vue-tinymce :content.sync="content" :config="config" />
     <h2>output</h2>
     <div v-html="content"></div>
   </div>
 </template>
 
 <script>
-    // require('../../dist/vue-tinymce.min')
- require('../../src')
-  //    import Vue from 'vue'
-  //    import VueTinymce from '../../dist/vue-tinymce.min'
-  //
-  //    Vue.use(VueTinymce)
+require('../../dist/vue-tinymce.min')
+// require("../../src");
+//    import Vue from 'vue'
+//    import VueTinymce from '../../dist/vue-tinymce.min'
+//
+//    Vue.use(VueTinymce)
 
-  //  const VueTinymce = () => import('../../dist/vue-tinymce.min')
+//  const VueTinymce = () => import('../../dist/vue-tinymce.min')
 
-  export default {
-    name: 'App',
-//    components: {VueTinymce},
-    data () {
-      return {
-        content: 'init content',
-        show: true,
-        locale: ''
+export default {
+  name: "App",
+  //    components: {VueTinymce},
+  data() {
+    return {
+      content: "init content",
+      show: true,
+      locale: ""
+    };
+  },
+  computed: {
+    config() {
+      let language = {};
+      if (this.locale === "en") {
+        language = { language: "en_US" };
       }
-    },
-    computed: {
-      config () {
-        let language = {}
-        if (this.locale === 'en') {
-          language = {language: 'en_CA'}
-        }
-        return language
-      }
-    },
-    created () {},
-    methods: {
-      switchLanguage () {
-        if (this.locale) {
-          this.locale = ''
-        } else {
-          this.locale = 'en'
-        }
+      return language;
+    }
+  },
+  created() {},
+  methods: {
+    switchLanguage() {
+      if (this.locale) {
+        this.locale = "";
+      } else {
+        this.locale = "en";
       }
     }
   }
+};
 </script>
-<style>
-</style>
+<style></style>
--- src/VueTinymce.vue
+++ src/VueTinymce.vue
@@ -4,150 +4,186 @@
   </div>
 </template>
 <script>
-  import tinymce from 'tinymce'
+import tinymce from "tinymce";
 
-  export default {
-    name: 'VueTinymce',
-    props: {
-      // 父组件通过:content.sync同步富文本编辑器内容
-      content: {
-        type: String,
-        required: true
-      },
-      // 触发content同步更新的tinymce Editor Events，其他https://www.tinymce.com/docs/advanced/events/
-      updateEvent: {
-        type: String,
-        default: 'beforeaddundo undo redo keyup'
-      },
-      // tinymce依赖文件的cdn url
-      url: {
-        type: String,
-        default: 'https://cdn.jsdelivr.net/npm/tinymce@4.9.3'
-      },
-      // tinymce的init方法的config参数，本组件有默认设置，比如不要toolbar3，可以使用该组件时写上 :config="{toolbar2:''}"
-      config: {
-        type: Object,
-        default () {
-          return {}
-        }
+export default {
+  name: "VueTinymce",
+  props: {
+    // 父组件通过:content.sync同步富文本编辑器内容
+    content: {
+      type: String,
+      required: true
+    },
+    // 触发content同步更新的tinymce Editor Events，其他https://www.tinymce.com/docs/advanced/events/
+    updateEvent: {
+      type: String,
+      default: "beforeaddundo undo redo keyup"
+    },
+    // tinymce依赖文件的cdn url
+    url: {
+      type: String,
+      default: "https://cdn.jsdelivr.net/npm/tinymce@latest"
+    },
+    // tinymce的init方法的config参数，本组件有默认设置，比如不要toolbar3，可以使用该组件时写上 :config="{toolbar2:''}"
+    config: {
+      type: Object,
+      default() {
+        return {};
       }
+    }
+  },
+  data() {
+    return {
+      editor: null
+    };
+  },
+  watch: {
+    config: {
+      handler: "init",
+      immediate: true,
+      deep: true
     },
-    data () {
-      return {
-        editor: null
+    content: {
+      handler: "setContent",
+      immediate: true
+    }
+  },
+  beforeDestroy() {
+    this.destroy();
+  },
+  methods: {
+    init() {
+      this.destroy();
+
+      const tinymceConfig = {
+        allow_script_urls: true,
+        remove_script_host: false,
+        convert_urls: false,
+        relative_urls: false,
+        // document_base_url: this.url,
+        // theme_url: `${this.url}/themes/silver/theme.min.js`,
+        // skin_url: `${this.url}/skins/ui/oxide`,
+        branding: false,
+        menubar: false,
+        fontsize_formats: "12px 13px 14px 15px 16px 18px 20px 24px",
+        external_plugins: {},
+        plugins:
+          "code hr link advlist lists paste table image imagetools media preview",
+        contextmenu: "selectall copy paste inserttable",
+        toolbar1:
+          "code | undo redo | fontsizeselect fontselect | blockquote hr | removeformat link unlink pastetext preview | image media",
+        toolbar2:
+          "bold italic underline strikethrough | forecolor backcolor | indent outdent | alignleft aligncenter alignright alignjustify | bullist numlist"
+      };
+
+      // 用外部配置覆盖内部默认配置
+      Object.assign(tinymceConfig, this.config);
+
+      // ============================================================================
+      // 如果语言相关为默认英语，则修改默认配置为中文
+
+      const zhCN = "zh_CN";
+      const enUS = "en_US";
+      // 如果语言没有配置，则默认配置为中文
+      if (!tinymceConfig.language) {
+        tinymceConfig.language = zhCN;
       }
-    },
-    watch: {
-      config: {
-        handler: 'init',
-        immediate: true,
-        deep: true
-      },
-      content: {
-        handler: 'setContent',
-        immediate: true
+      // 如果有配置语言，并且不是"en_US"，并且没有配置language_url，则使用本项目的语言包
+      if (
+        Object.prototype.toString.call(tinymceConfig.language) ===
+          "[object String]" &&
+        tinymceConfig.language !== enUS &&
+        Object.prototype.toString.call(tinymceConfig.language_url) !==
+          "[object String]"
+      ) {
+        tinymceConfig.language_url = `https://cdn.jsdelivr.net/npm/@panhezeng/vue-tinymce@latest/src/langs/${
+          tinymceConfig.language
+        }.min.js`;
       }
+
+      // 如果语言为中文，并且没有配置字体，则使用内部配置
+      if (
+        tinymceConfig.language === zhCN &&
+        Object.prototype.toString.call(tinymceConfig.font_formats) !==
+          "[object String]"
+      ) {
+        tinymceConfig.font_formats =
+          '微软雅黑="微软雅黑";苹方="苹方";宋体="宋体";黑体="黑体";仿宋="仿宋";楷体="楷体";隶书="隶书";幼圆="幼圆";Andale Mono=andale mono,times;Arial=arial,helvetica,sans-serif;Arial Black=arial black,avant garde;Book Antiqua=book antiqua,palatino;Comic Sans MS=comic sans ms,sans-serif;Courier New=courier new,courier;Georgia=georgia,palatino;Helvetica=helvetica;Impact=impact,chicago;Symbol=symbol;Tahoma=tahoma,arial,helvetica,sans-serif;Terminal=terminal,monaco;Times New Roman=times new roman,times;Trebuchet MS=trebuchet ms,geneva;Verdana=verdana,geneva;Webdings=webdings;Wingdings=wingdings,zapf dingbats';
+        // tinymceConfig.content_style =
+        //   'body, td, pre {font-family:"微软雅黑", "苹方", "宋体", Verdana, Arial, Helvetica, sans-serif;}';
+      }
+
+      // 如果配置为默认英语，则删除语言相关配置节点
+      if (tinymceConfig.language === enUS) {
+        delete tinymceConfig.language;
+        delete tinymceConfig.language_url;
+      }
+
+      // 把配置的plugins转换为external_plugins的形式，使用cdn
+      // if (
+      //   Object.prototype.toString.call(tinymceConfig.plugins) === "[object String]"
+      // ) {
+      //   const plugins = tinymceConfig.plugins.match(/([\d\w]+)/gm);
+      //   if (Object.prototype.toString.call(plugins) === "[object Array]") {
+      //     plugins.forEach(value => {
+      //       tinymceConfig.external_plugins[value] = `${
+      //         this.url
+      //       }/plugins/${value}/plugin.min.js`;
+      //     });
+      //   }
+      // }
+      // delete tinymceConfig.plugins;
+
+      // 从指定url加载tinymce依赖文件
+      tinymce.EditorManager.baseURL = this.url;
+
+      this.$nextTick(() => {
+        // 编辑器实例初始化
+        tinymceConfig.target = this.$refs.editor;
+        tinymceConfig.init_instance_callback = editor => {
+          if (
+            Object.prototype.toString.call(
+              this.config.init_instance_callback
+            ) === "[object Function]"
+          ) {
+            this.config.init_instance_callback(editor);
+          }
+          this.editor = editor;
+          this.setContent();
+          editor.on(
+            this.updateEvent,
+            tinymce.util.Delay.debounce(() => {
+              this.contentChange();
+            }, 300)
+          );
+        };
+        tinymce.init(tinymceConfig);
+      });
     },
-    beforeDestroy () {
-      this.destroy()
-    },
-    methods: {
-      init () {
-        this.destroy()
-
-        let config = {}
-
-        if (Object.prototype.toString.call(this.config) === '[object Object]' && Object.keys(this.config).length) {
-          config = Object.assign(config, this.config)
-        }
-
-        const zhCN = 'zh_CN'
-
-        // 如果language不等于false，才设置默认值
-        if (config.language !== false) {
-          if (Object.prototype.toString.call(config.language) !== '[object String]') {
-            config.language = zhCN
-          }
-          if (Object.prototype.toString.call(config.language_url) !== '[object String]') {
-            config.language_url = `https://cdn.jsdelivr.net/npm/@panhezeng/vue-tinymce@latest/src/langs/${config.language}.min.js`
-          }
-        }
-
-        const extend = {
-          font_formats: 'Arial=arial,helvetica,sans-serif;Arial Black=arial black,avant garde;Book Antiqua=book antiqua,palatino;Courier New=courier new,courier;Helvetica=helvetica;Symbol=symbol;Times New Roman=times new roman,times;Verdana=verdana,geneva;'
-        }
-
-        if (config.language === zhCN) {
-          extend.font_formats = '微软雅黑="微软雅黑";苹方="苹方";宋体="宋体";黑体="黑体";仿宋="仿宋";楷体="楷体";隶书="隶书";幼圆="幼圆";' + extend.font_formats
-          extend.content_style = 'body, td, pre {font-family:"微软雅黑", "苹方", "宋体", Verdana, Arial, Helvetica, sans-serif;}'
-        }
-
-        // 合并配置，把传入的配置和默认配置合并
-        config = Object.assign({
-          convert_urls: false,
-          branding: false,
-          theme_url: `${this.url}/themes/modern/theme.min.js`,
-          skin_url: `${this.url}/skins/lightgray`,
-          menubar: false,
-          fontsize_formats: '12px 13px 14px 15px 16px 18px 20px 24px',
-          external_plugins: {},
-          plugins: 'code hr link textcolor colorpicker advlist lists contextmenu paste table image imagetools media preview',
-          contextmenu: 'selectall copy paste inserttable',
-          toolbar1: 'code | undo redo | fontsizeselect fontselect | blockquote hr | removeformat link unlink pastetext preview | image media',
-          toolbar2: 'bold italic underline strikethrough | forecolor backcolor | indent outdent | alignleft aligncenter alignright alignjustify | bullist numlist'
-        }, extend, config)
-
-        // 把配置的plugins转换为external_plugins的形式，使用cdn
-        if (Object.prototype.toString.call(config.plugins) === '[object String]') {
-          const plugins = config.plugins.match(/([\d\w]+)/gm)
-          if (Object.prototype.toString.call(plugins) === '[object Array]') {
-            plugins.forEach(value => {
-              config.external_plugins[value] = `${this.url}/plugins/${value}/plugin.min.js`
-            })
-          }
-        }
-        delete config.plugins
-
-        this.$nextTick(() => {
-          // 编辑器实例初始化
-          config.target = this.$refs.editor
-          config.init_instance_callback = editor => {
-            if (Object.prototype.toString.call(this.config.init_instance_callback) === '[object Function]') {
-              this.config.init_instance_callback(editor)
-            }
-            this.editor = editor
-            this.setContent()
-            editor.on(this.updateEvent, tinymce.util.Delay.debounce(() => {
-              this.contentChange()
-            }, 300))
-          }
-          tinymce.init(config)
-        })
-      },
-      destroy () {
-        try {
-          // 销毁
-          if (this.editor) {
-            this.editor.remove()
-            this.editor.destroy()
-            this.editor = null
-          }
-        } catch (e) {}
-      },
-      setContent () {
-        // 如果编辑器实例已经为真，并且编辑器内容和父组件传入的内容不一样
-        if (this.editor && this.editor.getContent() !== this.content) {
-          this.editor.setContent(this.content)
-        }
-      },
-      contentChange () {
-        // 同步到父组件
+    destroy() {
+      try {
+        // 销毁
         if (this.editor) {
-          const content = this.editor.getContent()
-          this.$emit('update:content', content)
-          this.$emit('content-change', content)
+          this.editor.remove();
+          this.editor.destroy();
+          this.editor = null;
         }
+      } catch (e) {}
+    },
+    setContent() {
+      // 如果编辑器实例已经为真，并且编辑器内容和父组件传入的内容不一样
+      if (this.editor && this.editor.getContent() !== this.content) {
+        this.editor.setContent(this.content);
+      }
+    },
+    contentChange() {
+      // 同步到父组件
+      if (this.editor) {
+        const content = this.editor.getContent();
+        this.$emit("update:content", content);
+        this.$emit("content-change", content);
       }
     }
   }
+};
 </script>
